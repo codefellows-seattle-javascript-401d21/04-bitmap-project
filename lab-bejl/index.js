8d9c6b1b738ee040c860ae8918ab53559a32dbd0
Jordan Van Ness <jordanwvn@gmail.com>
2018-01-18 14:51:52 -0800

    added exports and requires to link files

--- lab-bejl/index.js
+++ lab-bejl/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const reader = require ('./lib/bitmap.js');
+const reader = require ('./lib/reader.js');
+const reader = require ('./lib/transform.js');
--- lab-bejl/lib/reader.js
+++ lab-bejl/lib/reader.js
@@ -1,1 +1,3 @@
 'use strict';
+
+const reader = modules.exports = {};
