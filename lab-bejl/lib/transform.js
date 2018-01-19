78a4efaf531f9c35b99707b7e9ed461acb20fd0f
Ender Smith <Ender Smith>
2018-01-19 11:56:14 -0800

    refactored transform & reader, still hardcoding targets

--- lab-bejl/lib/transform.js
+++ lab-bejl/lib/transform.js
@@ -3,29 +3,35 @@
 const transform = module.exports = {};
 const fs = require('fs');
 
-transform.black = function(input, callback) {
-  //gain?
-  for (let x = 0; x < input.pixelArray.length; x++) {
-    if (input.pixelArray[x]>50) {
-      input.pixelArray[x]=input.pixelArray[x] - 50;
-    }
+transform.darkenDarks = function(input) {
+  for ( let x = 0; x < input.pixelArray.length; x++ ) {
+    if ( input.pixelArray[x] > 50 ) {
+      input.pixelArray[x] = input.pixelArray[x] - 50;
+    };
   };
-  // //gain?
-  // for (let x = 0; x < input.pixelArray.length; x++) {
-  //     input.pixelArray[x]=input.pixelArray[x]-100;
-  // }
-  // //invert
-  // for (let x = 0; x < input.pixelArray.length; x++) {
-  //     input.pixelArray[x]=255-input.pixelArray[x];
-  // }
-  //noise
-  // for (let x = 0; x < input.pixelArray.length; x++) {
-  //     input.pixelArray[x]=input.pixelArray[x]-Math.floor(256*Math.random());
-  // }
-
-  fs.writeFile('./limitedchange.bmp', input.allData, (err, data) => {
-    if (err) console.log(err);
-    return data;
-  });
-  callback(null, input);
+  return input;
+}
+transform.neon = function(input) {
+  for ( let x = 0; x < input.pixelArray.length; x++ ) {
+      input.pixelArray[x] = input.pixelArray[x] - 100;
+  };
+  return input;
+};
+transform.invertColors = function(input) {
+  for ( let x = 0; x < input.pixelArray.length; x++ ) {
+      input.pixelArray[x] = 255-input.pixelArray[x];
+  };
+  return input;
+};
+transform.noise = function(input) {
+  for ( let x = 0; x < input.pixelArray.length; x++ ) {
+    input.pixelArray[x] = 255-input.pixelArray[x];
+  };
+  return input;
+};
+transform.black = function(input) {
+  for ( let x = 0; x < input.pixelArray.length; x++ ) {
+    input.pixelArray[x] = 0;
+  };
+  return input;
 };
