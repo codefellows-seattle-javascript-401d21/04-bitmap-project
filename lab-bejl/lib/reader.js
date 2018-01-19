ff26fe5e91ab8eabcbc22bb28bd5072281808976
billodell <billodell5@gmail.com>
2018-01-18 18:37:03 -0800

    testing writefile

--- lab-bejl/lib/reader.js
+++ lab-bejl/lib/reader.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const bitmap = require('./bitmap.js');
+const Bitmap = require('./bitmap.js');
 const transform = require('./transform.js');
 const reader = module.exports = {};
 
@@ -10,12 +10,14 @@ reader.readFile = function () {
   fs.readFile('../assets/bitmap.bmp', (err, data) => {
     if (err) console.error(err);
     // console.log(data);
-    const input = bitmap.breakIt(data);
-
-    console.log(input.pixelArray);
-    transform.black(input.pixelArray, (err,data) => {
+    const input = new Bitmap(data);
+    // console.log('inside reader',input.length);
+    // console.log('inside reader',input.offset);
+    // console.log('inside reader',input.width)
+    // console.log(input.);
+    transform.black(input, (err,data) => {
       if (err) console.error(err);
-      console.log(input);
+    //   console.log();
     });
   });
 };
--- lab-bejl/lib/transform.js
+++ lab-bejl/lib/transform.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const transform = module.exports = {};
+const fs = require('fs');
 
-transform.black = function(pixelArray, callback) {
-//   for (let x in pixelArray) {
-//     pixelArray[x] = 0;
+transform.black = function(input, callback) {
+//   for (const value of input.allData.values()) {
+//     // input[value] = 0;
+//     console.log(input.allData[value]);
 //   }
-  for (const value of pixelArray.values()) {
-    pixelArray[value] = 0;
-    console.log(pixelArray[value]);
-  }
-  callback(null, pixelArray);
+//   console.log('alldata',input);
+  //   console.log('offset',input.offset);
+  //   console.log('length',input.length);
+  //   console.log('width',input.width);
+  input.allData.fill(250, input.offset, input.length);
+  fs.writeFile('./test2.bmp',input.allData, (err, data) => {
+    if (err) console.log(err);
+    return data;
+  });
+  callback(null, input);
 };
 
