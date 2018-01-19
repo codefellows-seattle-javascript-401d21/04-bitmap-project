857370a06644f2091193c6def571df6cee9993a2
Ender Smith <Ender Smith>
2018-01-18 20:33:16 -0800

    got it

--- lab-bejl/lib/transform.js
+++ lab-bejl/lib/transform.js
@@ -4,22 +4,24 @@ const transform = module.exports = {};
 const fs = require('fs');
 
 transform.black = function(input, callback) {
-//   for (const value of input.allData.values()) {
-//     // input[value] = 0;
-//     console.log(input.allData[value]);
-//   }
-//   console.log('alldata',input);
-  //   console.log('offset',input.offset);
-  //   console.log('length',input.length);
-  //   console.log('width',input.width);
-
-  // input.allData.fill('FF', input.offset, input.offset+2000, 'utf-8');
-
-  // for (let x = input.allData.length; x < input.allData.length; x+4) {
-  //     input.allData[x]=255;
+  //gain?
+  for (let x = 0; x < input.pixelArray.length; x++) {
+    if (input.pixelArray[x]>50) {
+      input.pixelArray[x]=input.pixelArray[x] - 50;
+    }
+  };
+  // //gain?
+  // for (let x = 0; x < input.pixelArray.length; x++) {
+  //     input.pixelArray[x]=input.pixelArray[x]-100;
   // }
-
-  input.pixelArray.fill('FF', 'hex');
+  // //invert
+  // for (let x = 0; x < input.pixelArray.length; x++) {
+  //     input.pixelArray[x]=255-input.pixelArray[x];
+  // }
+  //noise
+  // for (let x = 0; x < input.pixelArray.length; x++) {
+  //     input.pixelArray[x]=input.pixelArray[x]-Math.floor(256*Math.random());
+  // }
 
   fs.writeFile('./limitedchange.bmp', input.allData, (err, data) => {
     if (err) console.log(err);
