0ca53d88c9f072de82a8984061557492d0169e22
Victoria Oh <veoh1989@gmail.com>
2018-01-19 17:50:18 -0800

    moved reader.js call to index.js

--- lab-bejl/lib/reader.js
+++ lab-bejl/lib/reader.js
@@ -5,14 +5,10 @@ const Bitmap = require('./bitmap.js');
 const transform = require('./transform.js');
 const reader = module.exports = {};
 
-// let path = process.argv[2];
-// let destination = process.argv[3];
-// let transformation = process.argv[4];
-
 reader.readFile = function (path, transformation, destination) {
   console.log(path, destination, transformation);
   if (typeof transformation !== 'string') return null;
-  fs.readFile(path, (err, data) => {  
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.error(err);
     }
@@ -27,7 +23,5 @@ reader.readFile = function (path, transformation, destination) {
   });
 };
 
-reader.readFile(`${process.argv[2]}`, `${process.argv[3]}`, `${process.argv[4]}`);
+//reader.readFile(`${process.argv[2]}`, `${process.argv[3]}`, `${process.argv[4]}`);
 // node reader.js '../assets/bitmap.bmp' 'black' './limitedchange.bmp'
-// process.argv[2]
-// './limitedchange.bmp'
