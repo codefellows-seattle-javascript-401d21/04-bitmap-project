79d8fddc346a2910e7e5909ef6c35ae20e7e7079
Ender Smith <Ender Smith>
2018-01-20 11:46:53 -0800

    also, renamed index.js

--- lab-bejl/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reader = require ('./lib/reader.js');
-
-reader.readFile(`${process.argv[2]}`, `${process.argv[3]}`, `${process.argv[4]}`);
-// node reader.js ../assets/bitmap.bmp black ./limitedchange.bmp
-
