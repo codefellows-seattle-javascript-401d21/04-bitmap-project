8e1460254b2c9b0806843e257680b0338a73c668
heath smith <heathsmith73@gmail.com>
2018-01-21 12:51:19 -0800

    ya

--- lab-hskprdjw/__test__/reader.test.js
+++ lab-hskprdjw/__test__/reader.test.js
@@ -9,8 +9,10 @@ require ('jest');
 // let setup = new Date();
 // let time = setup.toUTCString();
 
-let newPath = `${__dirname}/../assets/changed.bmp`;
+
 let startPath = `${__dirname}/../assets/bitmap.bmp`;
+let newPath = `${__dirname}/../assets/changed.bmp`;
+
 
 describe('index.js module', function() {
   //these are good
@@ -40,29 +42,29 @@ describe('index.js module', function() {
   });
 });
 
-describe('reader.js module', function() {
-  // //these are good
-  // it('test to see if anything was passed into the function', (done) => {
-  //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
-  //   expect(transform.reverseImage()).toBe(null);
-  //   expect(transform.removeBlue()).toBe(null);
-  //   expect(transform.boostBlue()).toBe(null);
-  //   expect(transform.randomImage()).toBe(null);
-  //   done();
-  // });
-  // it('test to see if the function worked and finished', (done) => {
-  //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
-  //   expect(build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue'])).toBe('file was writen');
-  //   done();
-  // });
-  // it('test to see if a file was made and if it matches what i called it', (done) => {
-  //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
-  //   expect(fs.existsSync(newPath)).toBe(true);
-  //   done();
-  // });
-  // it('test to see if a file is there that am calling to be referenced', (done) => {
-  //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
-  //   expect(fs.existsSync(startPath)).toBe(true);
-  //   done();
-  // });
-});
+// describe('reader.js module', function() {
+//   // //these are good
+//   // it('test to see if anything was passed into the function', (done) => {
+//   //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
+//   //   expect(transform.reverseImage()).toBe(null);
+//   //   expect(transform.removeBlue()).toBe(null);
+//   //   expect(transform.boostBlue()).toBe(null);
+//   //   expect(transform.randomImage()).toBe(null);
+//   //   done();
+//   // });
+//   // it('test to see if the function worked and finished', (done) => {
+//   //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
+//   //   expect(build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue'])).toBe('file was writen');
+//   //   done();
+//   // });
+//   // it('test to see if a file was made and if it matches what i called it', (done) => {
+//   //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
+//   //   expect(fs.existsSync(newPath)).toBe(true);
+//   //   done();
+//   // });
+//   // it('test to see if a file is there that am calling to be referenced', (done) => {
+//   //   build.buildImage(['node', 'index.js', 'bitmap', 'changed', 'removeBlue']);
+//   //   expect(fs.existsSync(startPath)).toBe(true);
+//   //   done();
+//   // });
+// });
